2d29a4e50ae2f168bb70d95368907c3a4913af90
Denskerz <de.errr18@gmail.com>
2024-05-15 10:36:28 +0300

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from ..database import Base
+from .stage_models import ConclusionOrm, CommonAttributesOrm, \
+    PLMothaballedUnfinObjOrm, PledgeGoodsOrm, PledgeSecuritiesOrm, PledgedVehiclesOrm, PledgePRRightAcquiredPROrm, \
+    PledgePRRightProceedsOrm, PledgePrRightLeasingOrm, PledgeFAInstalledEQOrm, PledgeImperfectUnpreservedOBJOrm
+from .log_models import LoginAuditOrm
+from .prom_models import UserOrm, RoleOrm
